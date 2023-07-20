a2459bf97637e4a6dca93921e53eaf687e2da89a
vvvdanilsss <shoodanyaters@gmail.com>
2023-07-20 21:09:41 +0300

    add contact form

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, flash
+import secrets
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secrets.token_hex(32)
 
-menu = ["Установка", "Первое приложение", "Обратная связь"]
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"}]
 
 
 @app.route("/")
-def index():  # put application's code here
+def index():
+    print(url_for('index'))
     return render_template('index.html', menu=menu)
 
 
 @app.route("/about")
 def about():
+    print(url_for('about'))
     return render_template('about.html', title="О сайте", menu=menu)
+
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        if len(request.form.get('username', default={})) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
+
+    return render_template('contact.html', title="Обратная связь", menu=menu)
 
 
 if __name__ == '__main__':
