fb128c199440b8ba609793611df3a76b2706199d
vvvdanilsss <shoodanyaters@gmail.com>
2023-07-20 21:41:10 +0300

    add login form

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, \
+    request, flash, session, redirect, abort
 import secrets
 
 app = Flask(__name__)
@@ -30,6 +31,30 @@ def contact():
             flash('Ошибка отправки', category='error')
 
     return render_template('contact.html', title="Обратная связь", menu=menu)
+
+
+@app.errorhandler(404)
+def pageNotFount(error):
+    return render_template('page404.html', title="Страница не найдена", menu=menu), 404
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+
+    return f'Профиль пользователя: {username}'
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session.get('userLogged', default='user')))
+    elif request.method == 'POST' and request.form.get('username') == "vvvdanilsss" and request.form.get('psw') == "1234":
+        session['userLogged'] = request.form.get('username')
+        return redirect(url_for('profile', username=session.get('userLogged')))
+
+    return render_template('login.html', title="Авторизация", menu=menu)
 
 
 if __name__ == '__main__':
